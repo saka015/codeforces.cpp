e65f2c93f7f1512abcbb2c3f6bf0136b6dffef15
Kamal Nayan <sknasylum8454@gmail.com>
2024-01-20 23:57:43 +0530

    pushed till 20 Jan

---
+++ 1374A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+    int x,y,n;
+    cin >> x >> y >> n;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        solve();
+    }
+}
